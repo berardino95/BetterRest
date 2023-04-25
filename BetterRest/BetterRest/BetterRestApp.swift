e15a291dd7965cca3ad2b42cfe189f7cb2c27915
Berardino Chiarello <berardino95@gmail.com>
2023-04-25 22:49:26 +0200

    Initial Commit

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by CHIARELLO Berardino - ADECCO on 25/04/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
